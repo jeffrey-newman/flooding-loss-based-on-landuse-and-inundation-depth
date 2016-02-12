ad838f8a867faf6fa8950433f4c450c6782070ea
Jeffrey Newman <jeffrey.newman@adelaide.edu.au>
2016-02-12 22:13:02 +1030

    Initial commit
    Initial commit of code. Using Alex’ Hagen-Zanker’s code for raster io.

---
+++ loss.hpp
@@ -0,0 +1,75 @@
+
+#ifndef LOSS_HPP
+#define LOSS_HPP
+
+#include "util/gdal_raster.h"
+#include "util/zip_range.h"
+#include <functional>
+#include <tuple>
+
+const double CELL_AREA = 0.09; //unit: hectares
+
+void calcLoss(raster_util::gdal_raster<int> & landuse,
+              raster_util::gdal_raster<double> & proportion,
+              raster_util::gdal_raster<double> & depth,
+              raster_util::gdal_raster<double> & loss_raster,
+              std::map<int, double> & loss_by_class,
+              double & net_loss)
+{
+    double loss = 0;
+    auto zip = raster_util::make_zip_range(std::ref(landuse), std::ref(proportion), std::ref(depth), std::ref(loss_raster));
+    for (auto i : zip)
+    {
+        auto& landuse_i = std::get<0>(i);
+        auto& proportion_i = std::get<1>(i);
+        auto& depth_i = std::get<2>(i);
+        auto& loss_raster_i = std::get<3>(i);
+        
+        loss = 0.0;
+        switch(landuse_i)
+        {
+            case 0: // high density residential
+                loss = -0.0484 * pow(depth_i,4) + 0.5509 * pow(depth_i,3) - 1.907 * pow(depth_i,2) + 3.5721 * depth_i;
+                break;
+            case 1: // low density residential
+                loss = -0.0271 * pow(depth_i,4) + 0.2921 * pow(depth_i,3) - 0.9387 * pow(depth_i,2) + 1.5621 * depth_i;
+                break;
+            case 2:   //inudstrial --- using commercial   (would infrastructure be better?, probably not)
+                loss = -0.0218 * pow(depth_i,4) + 0.2764 * pow(depth_i,3) - 0.8716 * pow(depth_i,2) + 1.36685 * depth_i;
+                break;
+            case 3:   // commercial
+                loss = -0.0218 * pow(depth_i,4) + 0.2764 * pow(depth_i,3) - 0.8716 * pow(depth_i,2) + 1.36685 * depth_i;
+                break;
+            case 4:   // Abandoned
+                loss = 0.0;
+                break;
+            case 5:   // Green, recereation, nbature and arable and horticulture were very similar. Using one of these.
+                loss = 0.0007 * pow(depth_i,4) - 0.0049 * pow(depth_i,3) - 0.0034 * pow(depth_i,2) + 0.1215 * depth_i;
+                break;
+            case 6:   // Facilities - again, no loss curves appropriate, will use commercial?
+                loss = -0.0218 * pow(depth_i,4) + 0.2764 * pow(depth_i,3) - 0.8716 * pow(depth_i,2) + 1.36685 * depth_i;
+                break;
+            case 7:   // Airport - again, no loss curves appropriate, what to do, as large part of area is ok if under inundation, but large secondary losses should airport not be able to be used? Should check Attorney Generals report for their economic evaluation.
+                loss = 0;
+                break;
+            case 8:   // Port - again, no loss curves appropriate...
+                loss = 0;
+                break;
+            case 9:   // Water - Loss 0 as this is where the water should be! :)
+                loss = 0;
+                break;
+        }
+        loss = loss * CELL_AREA * proportion_i;
+        loss_raster_i = loss;
+        loss_by_class[landuse_i] += loss;
+        net_loss += loss;
+        
+    }
+
+    
+}
+
+
+
+
+#endif //LOSS_HPP
---
+++ loss_calc.cpp
@@ -0,0 +1,109 @@
+
+#include <iostream>
+#include <fstream>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/graph/undirected_dfs.hpp>
+#include <boost/foreach.hpp>
+#include <boost/progress.hpp>
+
+#include "util/utility.h"
+#include "loss.hpp"
+
+
+
+int main(int argc, char **argv)
+{
+    
+    
+    /**********************************/
+    /*        Program options         */
+    /**********************************/
+    // Inputs
+    std::string landuse_map_file("no_file");
+    std::string proportion_flooded_file("no_file");
+    std::string flood_depth_file("no_file");
+   //Outputs
+    std::string loss_raster_file("out-raster.tif");
+    std::string out_list_file("loss-by-class-list.txt");
+    
+    namespace prog_opt = boost::program_options;
+    prog_opt::options_description desc("Allowed options");
+    desc.add_options()
+    ("help,h", "produce help message")
+    ("landuse,l", prog_opt::value<std::string>(&landuse_map_file), "path of the gdal capatible landuse file - using class symbology of CUDACCA - Blecic et al")
+    ("proportion-flooded,p", prog_opt::value<std::string>(&proportion_flooded_file), "path of the gdal compatible raster giving the proportion [0,1] of the cell which is flooded")
+    ("flood-depth,d", prog_opt::value<std::string>(&flood_depth_file), "path of the gdal compatible raster giving the average flood depth across the proportion flooded in the cell")
+    ("out-raster,r", prog_opt::value<std::string>(&loss_raster_file), "path where output tif raster of loss is saved")
+    ("out-list,o", prog_opt::value<std::string>(&out_list_file), "path where the output text file listing loss by class and total loss is saved");
+    
+    prog_opt::variables_map vm;
+    prog_opt::store(prog_opt::parse_command_line(argc, argv, desc), vm);
+    prog_opt::notify(vm);
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    
+    namespace fs = boost::filesystem;
+    
+    fs::path landuse_path(landuse_map_file);
+    fs::path proportion_path(proportion_flooded_file);
+    fs::path depth_path(flood_depth_file);
+    fs::path loss_path(loss_raster_file);
+    
+    
+    // Check file exists
+    if (!fs::exists(landuse_path))
+    {
+        std::stringstream ss;
+        ss << landuse_path << " does not exist";
+        throw std::runtime_error(ss.str());
+        return (EXIT_FAILURE);
+    }
+    
+    if (!fs::exists(proportion_path))
+    {
+        std::stringstream ss;
+        ss << proportion_path << " does not exist";
+        throw std::runtime_error(ss.str());
+        return (EXIT_FAILURE);
+    }
+    
+    if (!fs::exists(depth_path))
+    {
+        std::stringstream ss;
+        ss << depth_path << " does not exist";
+        throw std::runtime_error(ss.str());
+        return (EXIT_FAILURE);
+    }
+
+   
+    // open a raster data set
+    auto landuse = raster_util::open_gdal_raster<int>(landuse_path, GA_ReadOnly);
+    auto proportion = raster_util::open_gdal_raster<double>(proportion_path, GA_ReadOnly);
+    auto depth = raster_util::open_gdal_raster<double>(depth_path, GA_ReadOnly);
+    
+    // create a raster data set, with same dimensions as flood inundation maps
+    auto loss_raster = raster_util::create_gdal_raster_from_model<double>(loss_path, depth);
+    std::map<int, double> loss_by_class;
+    double net_loss;
+    
+    calcLoss(landuse, proportion, depth, loss_raster, loss_by_class, net_loss);
+    
+    std::ofstream list_fs(out_list_file);
+    if (list_fs.is_open())
+    {
+        list_fs << "total_loss\t" << net_loss << "\n";
+        list_fs << "class\tnet_loss_for_class\n";
+        for (auto& i : loss_by_class)
+        {
+            list_fs << i.first << "\t" << i.second << "\n";
+        }
+    }
+    
+}
